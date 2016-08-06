d2b7d73d629a729155f65eee440536aeea306726
Alex Gray <alex.j.gray23@gmail.com>
2016-08-06 15:59:57 -0400

    Add the wizard, it is now generic enough to be worked upon

---
+++ BreathJamApp/Assets/Scripts/DialogWizard.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+/// <summary>
+/// This compontent controls the flow of the wizard
+/// </summary>
+public class DialogWizard : MonoBehaviour
+{
+    public List<FormPage> pages;
+    int currentPage = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        pages = new List<FormPage>(this.GetComponentsInChildren<FormPage>());
+
+        for(int i = 0; i < pages.Count; i++ )
+        {
+            if (i != pages.Count - 1)
+            {
+                pages[i].nextButton.onClick.AddListener(() => { nextPage(); });
+            } else
+            {
+                pages[i].nextButton.onClick.AddListener(() => { finishWizard(); });
+                pages[pages.Count - 1].nextButton.GetComponentInChildren<Text>().text = "Finish";
+            }
+            pages[i].hide();
+            pages[i].transform.localPosition = new Vector3(0, 0, 0);
+        }
+
+        //Show the first page
+        pages[0].show();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void nextPage()
+    {
+        Debug.Log("Next page clicked");
+        pages[currentPage++].hide();
+        pages[currentPage].show();
+    }
+
+    void finishWizard ()
+    {
+        Debug.Log("Finish Wizard clicked");
+    }
+}
---
+++ BreathJamApp/Assets/Scripts/FormPage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// This component represents each form page
+/// </summary>
+public class FormPage : MonoBehaviour
+{
+    public Button nextButton;
+    public bool lastPage;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (lastPage)
+        {
+            nextButton.GetComponentInChildren<Text>().text = "Finish";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void hide()
+    {
+        this.GetComponent<CanvasGroup>().alpha = 0.0f;
+        this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void show()
+    {
+        this.GetComponent<CanvasGroup>().alpha = 1.0f;
+        this.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
